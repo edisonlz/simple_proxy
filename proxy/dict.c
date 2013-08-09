911ddc55d2848c28eb0869ad43332702e05f2506
liuzheng <edisonlz1@163.com>
2013-08-09 17:43:33 +0800

    fix bug

--- proxy/dict.c
+++ proxy/dict.c
@@ -14,7 +14,7 @@ connect_fd_struct *hash_find(int key) {
 void hash_set(int fd,int value) {
 
     connect_fd_struct *s = (connect_fd_struct*) malloc(sizeof(connect_fd_struct));
-    s->pair_fd = value;
+    strcpy(s->pair_fd , value);
     HASH_ADD_INT( fds, fd, s );
 
 }
